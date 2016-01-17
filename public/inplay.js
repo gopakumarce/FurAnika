c2c06497ca117a161fa7e301ebadb0c459049b00
Gopa Kumar <gopakumar.c.e@gmail.com>
2016-01-17 06:43:12 -0800

    Some more fixes

--- public/inplay.js
+++ public/inplay.js
@@ -59,7 +59,7 @@ function cellClickEvent(row, column){
     }
 
     cellSetColor(row, column, myColor);
-    inPlayFire[color].set({'row': row, 'column': column, color: 'myColor'});
+    inPlayFire[myColor].set({'row': row, 'column': column, color: 'myColor'});
     gameState = "TAKE_NEW_CARD";
     console.log('moving to take new card');
 }
